c094cb0643a259f5c9d6a8a0dd41aa5b06c52704
nicola-polito <nicolinobike122@gmail.com>
2026-05-19 10:16:27 +0200

    last updating version

--- E5/reco.py
+++ E5/reco.py
@@ -17,34 +17,36 @@ class Hit:
         self.id_detector=dect
         self.hit_time_rev= ht
 
+    # //
     def __repr__(self):
-        return 'Module: {:} __ Detector: {:} __ hit_time {:}'.format(self.id_module, self.id_detector, self.hit_time_rev)
+        return 'Module: {:} __ Detector: {:} __ hit_time: {:}'.format(self.id_module, self.id_detector, self.hit_time_rev)
     
     def __str__(self):
-        return 'Module: {:} -- Detector: {:} -- hit_time {:}'.format(self.id_module, self.id_detector, self.hit_time_rev)
+        return 'Module: {:} -- Detector: {:} -- hit_time: {:}'.format(self.id_module, self.id_detector, self.hit_time_rev)
 
-    def __eq__(self, other):
-        return self.hit_time_rev == other.hit_time_rev
-    
+
+    #COMPARATORI DI CONFRONTO
+
     def __lt__(self, other):
         return self.hit_time_rev < other.hit_time_rev
     
     def __gt__(self, other):
         return self.hit_time_rev > other.hit_time_rev
-    
-    def __sub__(self, other):
-        return self.hit_time_rev - other.hit_time_rev
-    
-    def __add__(self, other):
-        return self.hit_time_rev + other.hit_time_rev
 
-    def pos_module(self, other): #ordina 2 hit time in base al modulo dal quale sono stati rilevati
+    #//
+    def __eq__(self, other):
+        return self.hit_time_rev == other.hit_time_rev 
+
+    def pos_module(self, other): 
+        """ ordina 2 hit time in base al modulo dal quale sono stati rilevati """
         ord = False
         if(self.id_module<other.id_module or self.id_module==other.id_module):
             ord=True
         return ord
         
-    def pos_detector(self, other): #ordina 2 hit time in base al sensore dal quale sono stati rivelati (se sono relativi a 2 moduli diversi prevale il numero del modulo)
+    def pos_detector(self, other): 
+        """ ordina 2 hit time in base al sensore dal quale sono stati rivelati 
+            (se sono relativi a 2 moduli diversi, prevale il numero del modulo) """
         ord=False
         if(self.id_module == other.id_module):
             ord = self.id_detector < other.id_detector
@@ -52,13 +54,16 @@ class Hit:
             ord = self.pos_module(self,other)
         return ord
     
-    def ord_module(self, other): #ordina 2 hit time uguali in base al modulo dal quale sono stati rivelati.
+    def ord_module(self, other): 
+        """ ordina 2 hit time UGUALI in base al modulo dal quale sono stati rivelati. """
         ord = False
         if (self.hit_time_rev == other.hit_time_rev):
             ord= self.pos_module(self, other)
         return ord
         
-    def ord_detector(self, other): #ordina 2 hit time uguali in base al sensore dal quale sono stati rilevati(se sono relativi a due moduli diversi prevale il numero del modulo)
+    def ord_detector(self, other): 
+        """ ordina 2 hit time UGUALI in base al sensore dal quale sono stati rilevati
+            (se sono relativi a due moduli diversi, prevale il numero del modulo) """
         ord= False
         if (self.hit_time_rev == other.hit_time_rev):
             if(self.id_module == other.id_module):
@@ -66,6 +71,13 @@ class Hit:
             else:
                 ord = self.pos_module(self, other)              
         return ord
+    
+    #//
+    def __sub__(self, other):
+        return self.hit_time_rev - other.hit_time_rev
+    
+    def __add__(self, other):
+        return self.hit_time_rev + other.hit_time_rev
 
     
 
